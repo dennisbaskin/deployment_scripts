a805c290126df8185d55461fac4f6ef63b54e3f9
February <fskai@on-site.com>
2015-06-04 13:41:19 -0700

    Replace early exit when no build is desired

--- build_and_deploy_gem_to_s3.sh
+++ build_and_deploy_gem_to_s3.sh
@@ -15,7 +15,7 @@ CURRENT_SHA=`git rev-parse HEAD`
 TAGGED=`git tag --points-at=$CURRENT_SHA`
 if ! [[ $TAGGED =~ ^v[0-9] ]]; then
     echo "No Tagged version found."
-    #exit 0
+    exit 0
 fi
 echo "Tagged version found $TAGGED. Will build and sync."
 
